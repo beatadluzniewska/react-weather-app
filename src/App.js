5696ad586720b4d3e5be3346523bb69dadb06e8a
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2023-04-02 17:53:29 +0200

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>Weather app</h1>
+            <div className="container">
+                <h1>Weather app</h1>
+                <footer>
+                    <a
+                        href="https://github.com/beatadluzniewska/react-weather-app"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        open sourced on github
+                    </a>
+                </footer>
+            </div>
         </div>
     );
 }
