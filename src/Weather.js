45f0d3616f22f469b1d833fcc9b5f5c31db23c1a
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2023-04-10 12:19:04 +0200

    basic styling added

--- src/Weather.js
+++ src/Weather.js
@@ -29,18 +29,18 @@ export default function Weather() {
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
-                    <div className="clearfix">
-                        <img
-                            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                            alt="Mostly cloudy"
-                            className="float-left"
-                        />
+                    {/* <div className="clearfix"> */}
+                    <img
+                        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                        alt="Mostly cloudy"
+                        // className="float-left"
+                    />
 
-                        <div className="float-left">
-                            <span className="temperature">3</span>
-                            <span className="unit">°C</span>
-                        </div>
-                    </div>
+                    {/* <div className="float-left"> */}
+                    <span className="temperature">3</span>
+                    <span className="unit">°C</span>
+                    {/* </div> */}
+                    {/* </div> */}
                 </div>
                 <div className="col-6">
                     <ul>
