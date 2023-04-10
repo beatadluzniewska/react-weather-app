19b221c8a6c55389e1eb16fdd1f11163c86797b1
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2023-04-10 12:14:18 +0200

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -27,14 +27,20 @@ export default function Weather() {
                 <li>Wednesday 07:00</li>
                 <li>Mostly Cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img
-                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-                        alt="Mostly cloudy"
-                    />
-                    <span className="temperature">3</span>
-                    <span className="unit">°C</span>
+                    <div className="clearfix">
+                        <img
+                            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                            alt="Mostly cloudy"
+                            className="float-left"
+                        />
+
+                        <div className="float-left">
+                            <span className="temperature">3</span>
+                            <span className="unit">°C</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
