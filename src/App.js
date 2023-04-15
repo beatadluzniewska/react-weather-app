c795a112bdeb56a768adc53f3852f3e8ac663d92
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2023-04-15 16:51:13 +0200

    footer updated

--- src/App.js
+++ src/App.js
@@ -7,29 +7,20 @@ function App() {
             <div className="container">
                 <Weather defaultCity="Berlin" />
                 <footer>
-                    Project was coded by{" "}
+                    <a
+                        href="https://github.com/beatadluzniewska/react-weather-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Open-source code on GitHub{" "}
+                    </a>
+                    coded by{" "}
                     <a
                         href="https://github.com/beatadluzniewska/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         Beata Dluzniewska
-                    </a>
-                    , is{" "}
-                    <a
-                        href="https://github.com/beatadluzniewska/react-weather-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        open sourced on GitHub{" "}
-                    </a>
-                    and hosted on{" "}
-                    <a
-                        href="https://resonant-biscuit-6ff663.netlify.app/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Netlify
                     </a>
                 </footer>
             </div>
