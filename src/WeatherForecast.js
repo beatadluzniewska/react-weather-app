bd88db3d30ae6af3bc674bd898dcd61c2fb3adb4
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2026-01-11 20:31:48 +0100

    update API for weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,12 +15,13 @@ export default function WeatherForecast(props) {
         let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
         let longitude = props.coordinates.lon;
         let latitude = props.coordinates.lat;
-        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
     }
 
     function handleResponse(response) {
-        setForecast(response.data.daily);
+        console.log(response.data);
+        setForecast(response.data.list);
         setLoaded(true);
     }
 
@@ -29,17 +30,14 @@ export default function WeatherForecast(props) {
         return (
             <div className="WeatherForecast">
                 <div className="row">
-                    {forecast.map(function (dailyForecast, index) {
-                        if (index > 0 && index < 7) {
-                            return (
-                                <div className="col" key={index}>
-                                    <WeatherForecastDay data={dailyForecast} />
-                                </div>
-                            );
-                        } else {
-                            return null;
-                        }
-                    })}
+                    {forecast
+                        .filter((item, index) => index % 8 === 0)
+                        .slice(1, 6)
+                        .map((dailyForecast, index) => (
+                            <div className="col" key={index}>
+                                <WeatherForecastDay data={dailyForecast} />
+                            </div>
+                        ))}
                 </div>
             </div>
         );
